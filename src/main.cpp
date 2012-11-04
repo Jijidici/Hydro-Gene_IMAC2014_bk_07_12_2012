aef6c949219fb655ae27589e27478e4fe64b6255
CedricBidaud <cb1989@hotmail.fr>
2012-11-04 18:08:25 +0100

    fonction de test AABB triangle-box overlap OK avec un cube et une face, selon les axes x(1 0 0), y(0 1 0) et z(0 0 1)

--- src/main.cpp
+++ src/main.cpp
@@ -38,13 +38,13 @@ typedef struct{
 	GLdouble near;
 	int nbVertices;
 }Cube;
-
+/*
 typedef struct{
 	GLdouble x;
 	GLdouble y;
 	GLdouble z;
 }Axis;
-
+*/
 
 /******************************************/
 /*          FUNCTIONS                     */
@@ -72,6 +72,7 @@ Face createFace(Vertex* inS1, Vertex* inS2, Vertex* inS3){
 	return newFace;
 }
 
+/*
 Axis createAxis(GLdouble inX, GLdouble inY, GLdouble inZ){
 	Axis newAxis;
 	newAxis.x = inX;
@@ -80,22 +81,10 @@ Axis createAxis(GLdouble inX, GLdouble inY, GLdouble inZ){
 	
 	return newAxis;
 }
+*/
 
-Face projection(Face face, Axis axis){
-	
-	Axis projectionAxis = createAxis(0.,0.,0.);
-	
-	if(axis.x == 1.) projectionAxis = createAxis(0.,1.,1.);
-	if(axis.y == 1.) projectionAxis = createAxis(1.,0.,1.);
-	if(axis.z == 1.) projectionAxis = createAxis(1.,1.,0.);
-	
-	
-	Face projectedFace = createFace(face.s1,face.s2,face.s3);
-	
-	return projectedFace;
-}
-
-bool insideVertexTest(Cube cube, Face face){
+/*
+bool insideVertexTest(Cube cube, Face face){ // IT WORKS !
 	//cout << "cube.left : " << cube.left << " sommet.x : " << face.s1X << " cube.right : " << cube.right << endl;
 	//cout << "cube.bottom : " << cube.bottom << " sommet.y : " << face.s1Y << " cube.top : " << cube.top << endl;
 	if(face.s1->pos.x >= cube.left 		&& face.s1->pos.x <= cube.right
@@ -113,6 +102,165 @@ bool insideVertexTest(Cube cube, Face face){
 	
 	return false;
 }
+*/
+/** OPERATIONS **/
+glm::vec3 crossProduct(glm::vec3 v1, glm::vec3 v2){
+	glm::vec3 result;
+	result.x = v1.y*v2.z - v1.z*v2.y;
+	result.y = v1.z*v2.x - v1.x*v2.z;
+	result.z = v1.x*v2.y - v1.y*v2.x;
+	
+	return result;
+}
+
+GLdouble dotProduct(glm::vec3 v1, glm::vec3 v2){
+	return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+}
+
+glm::vec3 vecSub(glm::vec3 v1, glm::vec3 v2){
+	glm::vec3 result;
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	
+	return result;
+}
+
+/** MIN/MAX **/
+GLdouble min(GLdouble a, GLdouble b, GLdouble c){
+	GLdouble min = a;
+	if(b<min) min = b;
+	if(c<min) min = c;
+	
+	return min;
+}
+
+GLdouble max(GLdouble a, GLdouble b, GLdouble c){
+	GLdouble max = a;
+	if(b>max) max = b;
+	if(c>max) max = c;
+	
+	return max;
+}
+/************************/
+/******INTERSECTION******/
+/************************/
+
+bool aabbTriboxOverlapTest(Cube testedCube, Face testedFace){
+	size_t LEFT, RIGHT, TOP, BOTTOM;
+	LEFT = 0;
+	RIGHT = 1;
+	TOP = 2;
+	BOTTOM = 3;
+	
+	// projection selon les axes du repere
+	
+	// axe x(1 0 0) : //
+	GLdouble faceAABB[4]; // le plus en gauche/droite/haut/bas selon l'axe de projection
+	GLdouble cubeAABB[4];
+	//gauche
+	faceAABB[LEFT] = min(testedFace.s1->pos.z, testedFace.s2->pos.z, testedFace.s3->pos.z);
+	//droite
+	faceAABB[RIGHT] = max(testedFace.s1->pos.z, testedFace.s2->pos.z, testedFace.s3->pos.z);
+	//haut
+	faceAABB[TOP] = max(testedFace.s1->pos.y, testedFace.s2->pos.y, testedFace.s3->pos.y);
+	//bas
+	faceAABB[BOTTOM] = min(testedFace.s1->pos.y, testedFace.s2->pos.y, testedFace.s3->pos.y);
+	/*
+	cout << "plus à gauche selon x : " << faceAABB[0] << endl;
+	cout << "plus en haut selon x : " << faceAABB[2] << endl;
+	*/
+	cubeAABB[LEFT] = testedCube.far;
+	cubeAABB[RIGHT] = testedCube.near;
+	cubeAABB[TOP] = testedCube.top;
+	cubeAABB[BOTTOM] = testedCube.bottom;
+	/*
+	cout << "plus à gauche selon x : " << cubeAABB[0] << endl;
+	cout << "plus en haut selon x : " << cubeAABB[2] << endl;
+	*/
+	
+	// overlap test selon l'axe x
+	
+	if(faceAABB[LEFT] > cubeAABB[RIGHT] //triangle trop à droite
+	|| faceAABB[RIGHT] < cubeAABB[LEFT] //triangle trop à gauche
+	|| faceAABB[BOTTOM] > cubeAABB[TOP] //triangle trop en haut
+	|| faceAABB[TOP] < cubeAABB[BOTTOM]) //triangle trop en bas
+	{
+		cout << "test x" << endl;
+		return false; // pas d'overlap : on a un gap entre les deux projections selon l'axe x
+	}
+	/*
+	if(faceAABB[LEFT] > cubeAABB[RIGHT]){ cout << "LEFT" << endl; return false;}
+	if(faceAABB[RIGHT] < cubeAABB[LEFT]){ cout << "RIGHT" << endl; return false;}
+	if(faceAABB[BOTTOM] > cubeAABB[TOP]){ cout << "TOP" << endl; return false;}
+	if(faceAABB[TOP] < cubeAABB[BOTTOM]){ cout << "BOTTOM" << endl; return false;}
+	*/
+	// axe y(0 1 0) : //
+	faceAABB[LEFT] = min(testedFace.s1->pos.x, testedFace.s2->pos.x, testedFace.s3->pos.x);
+	//droite
+	faceAABB[RIGHT] = max(testedFace.s1->pos.x, testedFace.s2->pos.x, testedFace.s3->pos.x);
+	//haut
+	faceAABB[TOP] = min(testedFace.s1->pos.z, testedFace.s2->pos.z, testedFace.s3->pos.z);
+	//bas
+	faceAABB[BOTTOM] = max(testedFace.s1->pos.z, testedFace.s2->pos.z, testedFace.s3->pos.z);
+	/*
+	cout << "plus à gauche selon y : " << faceAABB[0] << endl;
+	cout << "plus en haut selon y : " << faceAABB[2] << endl;
+	*/
+	cubeAABB[LEFT] = testedCube.left;
+	cubeAABB[RIGHT] = testedCube.right;
+	cubeAABB[TOP] = testedCube.near;
+	cubeAABB[BOTTOM] = testedCube.far;
+	/*
+	cout << "plus à gauche selon y : " << cubeAABB[0] << endl;
+	cout << "plus en haut selon y : " << cubeAABB[2] << endl;
+	*/
+	
+	// overlap test selon l'axe y
+	if(faceAABB[LEFT] > cubeAABB[RIGHT] //triangle trop à droite
+	|| faceAABB[RIGHT] < cubeAABB[LEFT] //triangle trop à gauche
+	|| faceAABB[BOTTOM] > cubeAABB[TOP] //triangle trop en haut
+	|| faceAABB[TOP] < cubeAABB[BOTTOM]) //triangle trop en bas
+	{
+		cout << "test y" << endl;
+		return false; // pas d'overlap : on a un gap entre les deux projections selon l'axe y
+	}
+	
+	// axe z(0 0 1) : //
+	faceAABB[LEFT] = min(testedFace.s1->pos.x, testedFace.s2->pos.x, testedFace.s3->pos.x);
+	//droite
+	faceAABB[RIGHT] = max(testedFace.s1->pos.x, testedFace.s2->pos.x, testedFace.s3->pos.x);
+	//haut
+	faceAABB[TOP] = max(testedFace.s1->pos.y, testedFace.s2->pos.y, testedFace.s3->pos.y);
+	//bas
+	faceAABB[BOTTOM] = min(testedFace.s1->pos.y, testedFace.s2->pos.y, testedFace.s3->pos.y);
+	/*
+	cout << "plus à gauche selon z : " << faceAABB[0] << endl;
+	cout << "plus en haut selon z : " << faceAABB[2] << endl;
+	*/
+	cubeAABB[LEFT] = testedCube.left;
+	cubeAABB[RIGHT] = testedCube.right;
+	cubeAABB[TOP] = testedCube.top;
+	cubeAABB[BOTTOM] = testedCube.bottom;
+	/*
+	cout << "plus à gauche selon z : " << cubeAABB[0] << endl;
+	cout << "plus en haut selon z : " << cubeAABB[2] << endl;
+	*/
+	
+	// overlap test selon l'axe z
+	if(faceAABB[LEFT] > cubeAABB[RIGHT] //triangle trop à droite
+	|| faceAABB[RIGHT] < cubeAABB[LEFT] //triangle trop à gauche
+	|| faceAABB[BOTTOM] > cubeAABB[TOP] //triangle trop en haut
+	|| faceAABB[TOP] < cubeAABB[BOTTOM]) //triangle trop en bas
+	{
+		cout << "test z" << endl;
+		return false; // pas d'overlap : on a un gap entre les deux projections selon l'axe z
+	}
+	
+	return true;
+}
+
+
 
 static const Uint32 MIN_LOOP_TIME = 1000/FRAME_RATE;
 static const size_t WINDOW_WIDTH = 600, WINDOW_HEIGHT = 600;
@@ -164,6 +312,135 @@ int main(int argc, char** argv) {
 
 	fclose(fichier);
 	
+	// tableau de vertice envoyé au VBO
+	GLuint nbDisplayVertices = 3*nbFace;
+	Vertex* vertices = new Vertex[nbDisplayVertices];
+	int idx_vert = 0;
+	for(int n=0; n<nbFace; ++n){
+			vertices[idx_vert++] = *(tabF[n].s1);
+			vertices[idx_vert++] = *(tabF[n].s2);
+			vertices[idx_vert++] = *(tabF[n].s3);
+	}
+	
+	/***************************/
+	/***********TESTS***********/
+	/***************************/
+	/*
+	// test crossproduct
+	glm::vec3 v1, v2, vRes;
+	v1.x = 1.f; v1.y = 0.f; v1.z = 0.f;
+	v2.x = 0.f; v2.y = 1.f; v2.z = 0.f;
+	
+	vRes = crossProduct(v1,v2);
+	
+	cout << "crossProduct " << vRes.x << " " << vRes.y << " " << vRes.z << " " << endl;
+	
+	// test dotproduct
+	GLdouble dot = 0;
+	dot = dotProduct(v1, v2);
+	
+	cout << "dotProduct " << dot << endl;
+	
+	// test vecSub
+	glm::vec3 sub;
+	sub = vecSub(v1, v2);
+	cout << "vecSub " << sub.x << " " << sub.y << " " << sub.z << " " << endl;
+	*/
+	
+	
+	// CREATION D'UNE FACE DE TEST
+	Point aPoint, bPoint, cPoint;
+	aPoint.x = -0.3f;	aPoint.y = 0.4;		aPoint.z = 0.0f;
+	bPoint.x = 0.3f;	bPoint.y = -0.1;	bPoint.z = -0.2f;
+	cPoint.x = 0.2f;	cPoint.y = 0.1;		cPoint.z = 0.3f;
+	
+	aPoint.x = 1.f;		aPoint.y = 0.;		aPoint.z = 0.0f;
+	bPoint.x = 1.3f;	bPoint.y = 1.1;		bPoint.z = 1.2f;
+	cPoint.x = 1.2f;	cPoint.y = 1.1;		cPoint.z = 1.3f;
+	
+	Vertex aVertex;
+	aVertex.pos = aPoint;
+	Vertex bVertex;
+	bVertex.pos = bPoint;
+	Vertex cVertex;
+	cVertex.pos = cPoint;
+	
+	Face testFace = createFace(&aVertex, &bVertex, &cVertex);
+	
+	//Face testFace = tabF[0];
+	
+	// tableau de vertice envoyé au VBO
+	Vertex * faceVertice = new Vertex[3];
+	faceVertice[0] = *(testFace.s1);
+	faceVertice[1] = *(testFace.s2);
+	faceVertice[2] = *(testFace.s3);
+	
+	//cout << faceVertice[0].pos.x << endl;
+	
+	// CREATION DU CUBE DE TEST
+	Cube aCube = createCube(-0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f);
+	
+	GLdouble cubeVertices[] = {//
+		aCube.left, aCube.bottom, aCube.near,
+		aCube.right, aCube.bottom, aCube.near,
+		aCube.left, aCube.top, aCube.near,
+
+		aCube.right, aCube.bottom, aCube.near,
+		aCube.right, aCube.top, aCube.near,
+		aCube.left, aCube.top, aCube.near,
+
+		//
+		aCube.right, aCube.bottom, aCube.near,
+		aCube.right, aCube.bottom, aCube.far,
+		aCube.right, aCube.top, aCube.far,
+
+		aCube.right, aCube.bottom, aCube.near,
+		aCube.right, aCube.top, aCube.far,
+		aCube.right, aCube.top, aCube.near,
+
+		//
+		aCube.left, aCube.top, aCube.near,
+		aCube.right, aCube.top, aCube.near,
+		aCube.right, aCube.top, aCube.far,
+
+		aCube.left, aCube.top, aCube.near,
+		aCube.right, aCube.top, aCube.far,
+		aCube.left, aCube.top, aCube.far,
+
+		////
+		aCube.left, aCube.bottom, aCube.far,
+		aCube.right, aCube.bottom, aCube.far,
+		aCube.left, aCube.top, aCube.far,
+
+		aCube.right, aCube.bottom, aCube.far,
+		aCube.right, aCube.top, aCube.far,
+		aCube.left, aCube.top, aCube.far,
+
+		//
+		aCube.left, aCube.bottom, aCube.near,
+		aCube.left, aCube.bottom, aCube.far,
+		aCube.left, aCube.top, aCube.far,
+
+		aCube.left, aCube.bottom, aCube.near,
+		aCube.left, aCube.top, aCube.far,
+		aCube.left, aCube.top, aCube.near,
+
+		//
+		aCube.left, aCube.bottom, aCube.near,
+		aCube.right, aCube.bottom, aCube.near,
+		aCube.right, aCube.bottom, aCube.far,
+
+		aCube.left, aCube.bottom, aCube.near,
+		aCube.right, aCube.bottom, aCube.far,
+		aCube.left, aCube.bottom, aCube.far
+	};
+	
+	if(aabbTriboxOverlapTest(aCube, testFace)){
+		cout << "overlap" << endl;
+	}else{
+		cout << "no overlap" << endl;
+	}
+	
 	/* ************************************************************* */
 	/* *************INITIALISATION OPENGL/SDL*********************** */
 	/* ************************************************************* */
@@ -181,20 +458,10 @@ int main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 	
-	GLuint nbDisplayVertices = 3*nbFace;
-	Vertex* vertices = new Vertex[nbDisplayVertices];
-	int idx_vert = 0;
-	for(int n=0; n<nbFace; ++n){
-			vertices[idx_vert++] = *(tabF[n].s1);
-			vertices[idx_vert++] = *(tabF[n].s2);
-			vertices[idx_vert++] = *(tabF[n].s3);
-	}
-	
-	
 	/* ******************************** */
 	/* 		Creation des VBO, VAO 		*/
 	/* ******************************** */
-	
+
 	GLuint vbo = 0;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -209,12 +476,54 @@ int main(int argc, char** argv) {
 			glVertexAttribPointer(POSITION_LOCATION, 3, GL_DOUBLE, GL_FALSE, 0, NULL);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	
+	/************************/
+	/******FACE DE TEST******/
+	/************************/
+	
+	GLuint faceVBO = 0;
+	glGenBuffers(1, &faceVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, faceVBO);
+		glBufferData(GL_ARRAY_BUFFER, 3*sizeof(Vertex), faceVertice, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	GLuint faceVAO = 0;
+	glGenVertexArrays(1, &faceVAO);
+	glBindVertexArray(faceVAO);
+		glEnableVertexAttribArray(POSITION_LOCATION);
+		glBindBuffer(GL_ARRAY_BUFFER, faceVBO);
+			glVertexAttribPointer(POSITION_LOCATION, 3, GL_DOUBLE, GL_FALSE, 0, NULL);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+	/************************/
+	/******CUBE DE TEST******/
+	/************************/
+	
+	GLuint cubeVBO = 0;
+	glGenBuffers(1, &cubeVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	GLuint cubeVAO = 0;
+	glGenVertexArrays(1, &cubeVAO);  
+	glBindVertexArray(cubeVAO);  
+		glEnableVertexAttribArray(POSITION_LOCATION);
+		glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+			glVertexAttribPointer(POSITION_LOCATION, 3, GL_DOUBLE, GL_FALSE, 0, NULL);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+
 
 	// Creation des Shaders
 	GLuint program = imac2gl3::loadProgram("shaders/basic.vs.glsl", "shaders/basic.fs.glsl");
 	if(!program){
 		glDeleteBuffers(1, &vbo);
+		glDeleteBuffers(1, &faceVBO);
 		glDeleteVertexArrays(1, &vao);
+		glDeleteVertexArrays(1, &faceVAO);
 		return (EXIT_FAILURE);
 	}
 	glUseProgram(program);
@@ -266,8 +575,22 @@ int main(int argc, char** argv) {
 		MVP = glm::rotate(MVP, angleViewY + tmpAngleViewY,  glm::vec3(1.f, 0.f, 0.f)); //ROTATE WITH YCOORDS CLIC
 		glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, glm::value_ptr(MVP));
 		
+		
+		// face de test
+		glBindVertexArray(faceVAO);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+		
+		/*
+		// terrain
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, nbDisplayVertices);
+			glDrawArrays(GL_TRIANGLES, 0, nbDisplayVertices);
+		glBindVertexArray(0);
+		*/
+		
+		// cube de test (wireframe)
+		glBindVertexArray(cubeVAO);
+			glDrawArrays(GL_LINE_LOOP, 0, aCube.nbVertices);
 		glBindVertexArray(0);
 
 		// Mise à jour de l'affichage
@@ -401,11 +724,13 @@ int main(int argc, char** argv) {
 
 	// Destruction des ressources OpenGL
 	glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &faceVBO);
 	glDeleteVertexArrays(1, &vao);
-	
+	glDeleteVertexArrays(1, &faceVAO);
 	delete[] tabV;
 	delete[] tabF;
 	delete[] vertices;
+	delete[] faceVertice;
 
 	SDL_Quit();
 
